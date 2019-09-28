083977328b89af08b6fc1d09a825bb2405f2d83d
jinxing-star <jinxing97@126.com>
2019-09-28 17:53:26 +0800

    第三周作业

---
+++ P25035-Changsha-Zhouzhenxing/week03/word_counts.py
@@ -0,0 +1,28 @@
+﻿# 任一个英文的纯文本文件，统计其中的单词出现的个数
+
+
+def word_counts(file, word):
+    '''
+    :param file: 要搜索的文本文件
+    :param word: 要搜索的单词
+    :return: 返回单词在文本文件中出现的次数
+    '''
+    qty = 0     #word在每行中出现的次数
+    total = 0   #word在file中出现的总次数
+
+    with open(file, encoding="utf-8") as file:
+        while True:
+            str = file.readline()
+            if str == '':
+                break
+
+            qty = str.count(word)
+            total += qty
+
+    return total
+
+
+if __name__ == '__main__':
+    file = r".\Python之禅.txt"
+    word = "better"
+    print("“"+ word + "”" , "在文件 “" , file.replace(".\\", '') , "”中出现的次数为：" , word_counts(file, word))
