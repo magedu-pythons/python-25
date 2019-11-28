33bc24343ab467bccd2f2763494fa36da1c5674e
jinxing-star <jinxing97@126.com>
2019-11-28 10:21:21 +0800

    第三周作业

--- P25047-wangruochen/week/查找某元素是否在一个列表中.py
+++
@@ -1,9 +0,0 @@
-new = list(range(10))
-number = int(input("please input a number:"))
-print(list)
-counter = 0
-for i in new:
-    if i == number:
-        counter += 1
-if counter != 0:
-    print("1")
--- P25047-wangruochen/week/统计英文文本中的重复字母.py
+++
@@ -1,8 +0,0 @@
-wenjian = "d e d f s a a a a d s a f d s f s a"
-new = wenjian.split(" ")
-print(new)
-counter = 0
-for i in new:
-    if 'a' == i:
-        counter += 1
-print(counter)
