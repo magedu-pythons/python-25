78f37266496f230c09ace74495634c8b0d38730d
zzx201993 <478050094@qq.com>
2020-03-07 12:10:49 +0800

    第十三周作业

---
+++ P25035-Changsha-Zhouzhenxing/week13/timeit.py
@@ -0,0 +1,32 @@
+# 第十三周作业
+# 本周作业：实现一个timeit的装饰器：timeit 装饰能够计算被装饰函数的运行时间
+
+import datetime
+import time
+import functools
+
+
+def timeit(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        starttime = datetime.datetime.now()
+
+        ret = fn(*args, **kwargs)
+
+        endtime = datetime.datetime.now()
+
+        print('计算运行时间：{}秒'.format((endtime- starttime).total_seconds()) )
+
+        return ret
+
+    return wrapper
+
+
+
+@timeit
+def add(x:int, y:int) -> int:
+    time.sleep(1)
+    return x + y
+
+
+add(3, 3)
